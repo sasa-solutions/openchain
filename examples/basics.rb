de9ae75eda9b77739330985c98eb1fb47200abd0
renen <renen@121.co.za>
2020-09-30 11:03:20 +0200

    reflect revised thinking in sample prior to progressing entities

--- examples/basics.rb
+++ examples/basics.rb
@@ -1,5 +1,7 @@
 require_relative '../lib/open_chain.rb'
 
-person = OpenChain::Person.new(identifier: 48, name: '2kg Red Chilli Seeds')
-
+person = OpenChain::Person.new(identifier_type: 'msisdn', identifier: '27833939495', first_name: 'Renen')
 pp person.uuid
+
+# should be the same person.
+person = OpenChain::Person.new(identifier_type: 'uuid', identifier: person.uuid)
