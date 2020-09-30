d321bd3ace480dc33be24027b0cd49d49b202021
renen <renen@121.co.za>
2020-09-30 11:04:21 +0200

    concept parameters for location object. What do we assume by wrapping Googles model?

--- lib/open_chain/models/location.rb
+++ lib/open_chain/models/location.rb
@@ -1,3 +1,22 @@
 class Location
   
+  attr_reader :address, :latitude, :longitude, :uuid, :plus_code, :country, :administrative_area_level_1, :administrative_area_level_2, :locality, :sublocality, :postal_code
+  attr_writer :address, :latitude, :longitude
+
+  @uuid                        = nil
+
+  @address                     = nil
+  @latitude                    = nil
+  @longitude                   = nil
+
+  @plus_code                   = nil
+  @country                     = nil
+  @administrative_area_level_1 = nil
+  @administrative_area_level_2 = nil
+  @locality                    = nil
+  @sublocality                 = nil
+  @postal_code                 = nil
+
 end
+
+
